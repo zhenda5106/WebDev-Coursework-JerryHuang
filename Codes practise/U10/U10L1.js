84ce3eaf2e6741d035132754e249ee220b64a8ab
zhenda5106 <hziyang837@gmail.com>
2025-05-20 19:39:51 +0800

    Add files via upload

---
+++ Codes practise/U10/U10L1.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Welcome to the cat page!");
+
+    const meowButton = document.getElementById("meowButton");
+    meowButton.addEventListener("click", function () {
+        alert("😺");
+    });
+});
---
+++ Codes practise/U10/U10L3.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("🚀 Space page loaded. Prepare for liftoff!");
+});
